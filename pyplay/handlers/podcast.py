b7c94350a1117fe2b4c48f0adf2e3a556ae0a5bf
Ian McGaunn <ianmcgaunn@pressmail.ch>
2017-08-05 13:52:48 -0400

    Rename 'get' handler to 'info' and change method to POST
    also add missing test data

--- pyplay/handlers/podcast.py
+++ pyplay/handlers/podcast.py
@@ -1,7 +1,7 @@
 import json
 
 
-def get(ev, ctx):
+def info(ev, ctx):
     headers = ev['headers']
     body = ev['body']
 
@@ -13,23 +13,23 @@ def get(ev, ctx):
     }
 
 
-def init(sess_token, host=None):
-    """ all transactions need to pass around the session token
-    that was provided to init when client started.
-
-    when client first starts, it calls this method and supplies
-    its session token.
-
-    If a backend table exists for that session token, this method
-    returns a serialized version of the server's copy of the user's
-    state.
-
-    If not, create a new backend table for token and load a default
-    state.
-
-    """
-
-    pass
+# def init(sess_token, host=None):
+#    """ all transactions need to pass around the session token
+#    that was provided to init when client started.
+#
+#    when client first starts, it calls this method and supplies
+#    its session token.
+#
+#    If a backend table exists for that session token, this method
+#    returns a serialized version of the server's copy of the user's
+#    state.
+#
+#    If not, create a new backend table for token and load a default
+#    state.
+#
+#    """
+#
+#    pass
 
 
 def update_state(sess_id, state):
